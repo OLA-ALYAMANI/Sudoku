6beada47740da7730664112d8f087bffdc987237
OLA13 <o-m-yamani@hotmail.com>
2020-03-14 01:19:32 +0300

    My first game projrct for second time
    I add the time to be work if I click ti start game but it seems not to work.

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,62 @@
-// show and hide Pag
+$("#Go").click(function(){
+      if($(".Time").html() == 00){
+            start_time()
+      }
+})
+// Track our timer
+var active = false
+// the main function
+function start_time(){
+      if(active){
+            var timer = document.getElementsByClassName(".Timer").innerHTML
+            // split the time use (:) in the array
+            var arr = timer.split(":")
+            // get hour
+            var hour = arr[0]
+            // get minutes
+            var min = arr[1]
+            //  get seconds
+            var sec = arr[2]
+
+            if(min == 59){
+                  if(sec == 59){
+                        hour++
+                        min = 0
+                        // for the number of hour less then 10
+                        if(hour < 10)
+                              hour = "0" + hour
+                        }
+                        else{
+                              min++
+                        }
+                        // for the number of minutes less then 10
+                        if(min < 10)
+                              min = "0" + min
+                              sec = 0
+                        }
+                        else{
+                              sec++
+                        if(sec < 10)
+                              sec = "0" + sec
+                  }
+            // update html
+            document.getElementsByClassName(".Timer").innerHTML = hour + ":" + min + ":" + sec
+            setTimeout(start_time(), 1000)
+      }
+}
+// fuction for change states - if click play or pause
+      function changeState(){
+            if(active == false){
+                  document.getElementsByID("#pause")
+            }
+            else{
+                  active = true
+                        start_time()
+                  document.getElementsByID("#play")
+            }
+      }
+
+// show and hide Pags
 $(".game_Pag").hide();
 $("a").on("click", function(){
       $(".game_Pag").hide();
@@ -6,68 +64,47 @@ $("a").on("click", function(){
 });
 
 $("#Go").on("click", function(){
+      start_time()
       $(".game_Pag").show();
       $(".Front_Pag").hide();
 });
 
-//
 $("#instruc_game").hide();
 $("#About_Game").on("click", function(){
       $("#instruc_game").show();
 });
 
-// $(".call_num").on("click", function(){
-//        $(this).css({"border": "rgb(226, 139, 39) 3px solid"});
-//  });
 
-// $(".Val_Num").on("click", function(){
-//       $(this).stop(1000);
-// });
-// let time = 0
-// let TimeStart = document.querySelector("#time_start")
-// function GetTime(){
-// let data = new Date()
-// let min = data.getMinutes()
-// let sec = data.getSeconds()
+// var Sudoku=[[1,2,3,0],[4,0,2,0],[0,1,4,0],[0,0,0,2]]
 
-// TimeStart.textContent = min + " : " + sec
-// }
-// setInterval(GetTime, 1000)
+// var Sudoku_solve=[[1,2,3,4],[3,4,2,1],[2,1,4,3],[4,3,1,2]]
 
-var result = [];
+var Boxs
+var row = $(".Val_Num")
+var clou = $(".colm")
+var numbutton = $(".TakNum").children()
 
-function add_Value(resl){
-      console.log(resl);
-      for(let r=0; r<4; r++){
-            let values = document.querySelector("#But_item-"+r).onckilck()
-            for(let c=0; c<4; c++){ 
-                  var input = document.querySelector("#" + [r][c])
-                  $(input).on("click", function(){
-                        if(input.value === " ") { 
-                              input.css({"border": "rgb(226, 139, 39) 3px solid"})
-                              $(value).click(function()
-                              {
-                                    document.getElementById("#"+[r][c]).value = $("#But_item-" + r).val();
-                              });
-                        }
-                              else if("#"+[r][c] !== " "){
-                                    alert("This not empty box");
-                                    $(values).html("#"+[r][c]);
-                              }
-                        
-                  })
-            }
+// select the one of empty box
+row.click(function(e){
+      if($(this).text()==""){
+            $(this).addClass("select")
+            Boxs = $(this) 
       }
-}
+})
 
-// function loadUser(){
-//       for(let r=0; r<4; r++){
-//             for(let c=0; c<4; c++){
-//                   let callVal;
-//                   let n = "#" + r + c;
-//                   if($(n).find("#But_item-"+r)){
-//                         callVal = $(n).find("#But_item-"+[r]).val();
-//                   }
-//             }
-//       }
-// }
+// set the number to empty box
+numbutton.click(function(){
+      if(Boxs){
+            Boxs.val($(this).text())
+      }
+      for (var i = 1; i <= 4; i++)
+      var check = Boxs[0]
+      if(check == row[i] && check == clou[i]){
+            return false
+      }
+      else{
+            return true
+      }
+})
+// Boxs.attr('id').match(/\d+$/)
+// console.log($(this).attr("id"))
